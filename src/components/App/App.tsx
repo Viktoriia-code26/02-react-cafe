7bd3d49953de960c1e053b907993cd41c83992bf
Viktoriia P. <pohrebniakvika@gmail.com>
2025-09-17 19:54:22 +0200

    all files included

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,10 +1,10 @@
 import { useState } from "react";
 import type { Votes, VoteType } from "../types/votes";
 import css from "./App.module.css";
-import CafeInfo from "./CafeInfo";
-import VoteOptions from "./VoteOptions";
-import VoteStats from "./VoteStats";
-import Notification from "./Notification";
+import CafeInfo from "../CafeInfo/CafeInfo";
+import VoteOptions from "../VoteOptions/VoteOptions";
+import VoteStats from "../VoteStats/VoteStats";
+import Notification from "../Notification/Notification";
 
 export default function App() {
   const [votes, setVotes] = useState<Votes>({
